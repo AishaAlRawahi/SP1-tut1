7b418b00ec36075e72a56569f00740c4547c2c50
AishaAlRawahi <aishaalrawahi06@gmail.com>
2026-09-15 15:39:56 +0300

    Add fetchTodos function to API module

--- Tut1/frontend/src/api.js
+++ Tut1/frontend/src/api.js
@@ -4,3 +4,8 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
+
+export const fetchTodos = (done) => {
+  const params = done !== undefined ? { done } : {};
+  return api.get('/', { params });
+};
